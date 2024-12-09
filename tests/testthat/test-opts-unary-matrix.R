5ab6c0e8a9c2843e4fd049f95f5a5dc49737f01d
mike <mikefc@coolbutuseless.com>
2024-12-09 16:42:55 +1000

    clear up some errors

---
+++ tests/testthat/test-opts-unary-matrix.R
@@ -0,0 +1,69 @@
+
+test_that("matrix unary sqrt works", {
+  A0 <- matrix(as.numeric(1:6), 3, 2)
+  A1 <- duplicate(A0)
+  
+  br_sqrt(A1)
+  
+  expect_identical(
+    A1,
+    sqrt(A0)
+  )
+})
+
+
+
+test_that("matrix unary sqrt with idx works", {
+  A0 <- matrix(as.numeric(1:6), 3, 2)
+  A1 <- duplicate(A0)
+  
+  br_sqrt(A1, idx = c(1, 6))
+  
+  A0[1] <- sqrt(A0[1])
+  A0[6] <- sqrt(A0[6])
+  
+  expect_identical(
+    A1,
+    A0
+  )
+})
+
+
+
+test_that("matrix unary sqrt with 'where' works", {
+  A0 <- matrix(as.numeric(1:6), 3, 2)
+  A1 <- duplicate(A0)
+  
+  br_sqrt(A1, where = c(F, F, F, F, T, T))
+  
+  A0[5] <- sqrt(A0[5])
+  A0[6] <- sqrt(A0[6])
+  
+  expect_identical(
+    A1,
+    A0
+  )
+})
+
+
+
+test_that("matrix unary sqrt with 'where' of incorrect length fails", {
+  A0 <- matrix(as.numeric(1:6), 3, 2)
+  
+  expect_error(
+    br_sqrt(A0, where = c(F, F, F, T)),
+    "same length"
+  )
+})
+
+
+test_that("matrix unary sqrt with 'idx' out-of-bounds fails", {
+  A0 <- matrix(as.numeric(1:6), 3, 2)
+  
+  expect_error(
+    br_sqrt(A0, idx = c(7)),
+    "out-of-bounds"
+  )
+})
+
+
